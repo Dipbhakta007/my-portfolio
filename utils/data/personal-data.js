7942b1b4fa713acb12edf53ebf08901a2013ea42
Dip Bhakta <dipbhakta007@Dips-MacBook-Pro-2.local>
2025-05-16 23:31:20 +0600

    Update Experience and about section images

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,8 +1,8 @@
 export const personalData = {
   name: "DIP BHAKTA",
-  profile: '/profile.png',
+  profile: '/image/profile.png',
   designation: "Software Engineer",
-  description: "I am a Software Engineer with 4+ years of hands-on experience building secure, scalable infrastructure across telco, CRM, and advertising platforms. Expert in Kubernetes, CI/CD pipelines, and cloud platforms (AWS, GCP). Proven track record of optimizing delivery pipelines, ensuring system reliability (1800+ TPS), and implementing cloud-native architectures. AWS Certified, CKA, and CKS certified. Experienced in cross-functional collaboration and remote team environments.",
+  description: "I am a Software Engineer with 4+ years of hands-on experience building secure, scalable infrastructure across telco, sales & distribution, CRM, and advertising platforms. Expert in Kubernetes, CI/CD pipelines, and cloud platforms (AWS, GCP). Proven track record of optimizing delivery pipelines, ensuring system reliability (1800+ TPS), and implementing cloud-native architectures. AWS Certified, CKA, CKS, Hashicorp certified. Experienced in cross-functional collaboration and remote team environments.",
   email: 'bhaktadip@gmail.com',
   phone: '+8801833183520',
   address: 'Uttara, Dhaka, Bangladesh - 1230 ',
