7106a2a87608047aac66094eb80357df629b347e
Dip Bhakta <dipbhakta@Dips-MacBook-Pro.local>
2025-05-20 21:31:47 +0600

    fix dockerfile prod

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
-const path = require('path')
- 
+// next.config.js
+const path = require('path');
+
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
   images: {
     remotePatterns: [
+      // Allow media2.dev.to
       {
         protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        hostname: 'media2.dev.to',
         pathname: '**',
       },
       {
@@ -16,11 +18,18 @@ module.exports = {
         hostname: 'media.dev.to',
         pathname: '**',
       },
+      // Allow AWS S3 (where the image is actually hosted)
       {
         protocol: 'https',
-        hostname: 'media2.dev.to',
+        hostname: 'dev-to-uploads.s3.amazonaws.com',
+        pathname: '**',
+      },
+      // Other domains (e.g., Cloudinary)
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
         pathname: '**',
       },
     ],
   },
-}
+};
